f4538e28d54563ad25a247ddab98872f766f5548
985565 <985565@NCSLab_219-204.cs.mum.edu>
2017-04-04 17:08:51 -0500

    refac

--- labs/src/lab2/mapreduce/Main.java
+++ labs/src/lab2/mapreduce/Main.java
@@ -1,4 +1,4 @@
-package lab2;
+package lab2.mapreduce;
 
 import java.util.List;
 
--- labs/src/lab2/mapreduce/Pair.java
+++ labs/src/lab2/mapreduce/Pair.java
@@ -1,4 +1,4 @@
-package lab2;
+package lab2.mapreduce;
 
 public class Pair<K extends Comparable, V> implements Comparable<Pair> {
 
