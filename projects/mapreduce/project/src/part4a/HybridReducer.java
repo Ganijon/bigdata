e9872ab023339f890620292297758cc037011a0f
ganijon <ganijon.rahimov@gmail.com>
2017-04-24 16:50:59 -0700

    improved hybrid reducer

--- projects/mapreduce/project/src/part4a/HybridReducer.java
+++ projects/mapreduce/project/src/part4a/HybridReducer.java
@@ -1,78 +1,77 @@
 package part4a;
 
-/**
- *
- * @author ganijon
- */
-
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-import parts.Stripe;
-import parts.WordPair;
+import parts.*;
 
 public class HybridReducer extends Reducer<WordPair, IntWritable, Text, Stripe> {
-	
-	private Stripe stripes;
-	private Text prev;
-	private int total;
 
+	private HashMap<String, Integer> H;
+	private double total;
+	private String prev;
 
 	@Override
-	protected void setup(Reducer<WordPair, IntWritable, Text, Stripe>.Context context)
+	protected void setup(
+			Reducer<WordPair, IntWritable, Text, Stripe>.Context context)
 			throws IOException, InterruptedException {
 		super.setup(context);
-		
-		stripes = new Stripe();
+		H = new HashMap<String, Integer>();
 		prev = null;
 		total = 0;
 	}
-	
-	@Override
-	public void reduce(WordPair key, Iterable<IntWritable> values, Context context)
-			throws IOException, InterruptedException {
-		Text w = key.getWord();
-		Text u = key.getNeighbor();
 
-		if (w.equals(prev) && prev != null) {
-			updateStripes(stripes, total);
-			context.write(prev, stripes);
-			stripes.clear();
+	@Override
+	protected void reduce(WordPair pair, Iterable<IntWritable> counts,
+			Reducer<WordPair, IntWritable, Text, Stripe>.Context context)
+			throws IOException, InterruptedException {
+
+		String w = pair.getWord().toString();
+		String u = pair.getNeighbor().toString();
+
+		if (prev != null && !prev.equals(w)) {
+			context.write(new Text(prev), getUpdatedStripe());
+			H = new HashMap<String, Integer>();
 			total = 0;
-		}	
-		
-		int sum = sum(values);
+		}
+
+		int sum = sum(counts);
 		total += sum;
-		stripes.put(new Text(u.toString()), new DoubleWritable(sum));
+		H.put(u, sum);
 		prev = w;
 	}
 
 	@Override
-	protected void cleanup(Reducer<WordPair, IntWritable, Text, Stripe>.Context context)
-			throws IOException, InterruptedException {
+	protected void cleanup(Context context) throws IOException,
+			InterruptedException {
 		super.cleanup(context);
-		
-		updateStripes(stripes, total);
-		context.write(prev, stripes);
+		context.write(new Text(prev), getUpdatedStripe());
 	}
-	
+
+	private Stripe getUpdatedStripe() {
+		Stripe stripe = new Stripe();
+		DecimalFormat df = new DecimalFormat("#.###");
+		double frequency = 0.0;
+		for (Entry<String, Integer> entry : H.entrySet()) {
+			frequency = entry.getValue() / total;
+			stripe.put(new Text(entry.getKey()),
+					new DoubleWritable(Double.valueOf(df.format(frequency))));
+		}
+		return stripe;
+	}
+
 	private int sum(Iterable<IntWritable> values) {
 		int sum = 0;
-		for(IntWritable i: values)
-			sum += i.get();
+		for (IntWritable intWritable : values) {
+			sum += intWritable.get();
+		}
 		return sum;
 	}
-
-	private void updateStripes(Stripe stripes, int total) {
-		for (Writable key : stripes.keySet()) {
-			double newVal = ((DoubleWritable)stripes.get(key)).get();
-			double frequency = newVal / total;
-			stripes.put(key, new DoubleWritable(frequency));
-		}
-	}
-
 }
