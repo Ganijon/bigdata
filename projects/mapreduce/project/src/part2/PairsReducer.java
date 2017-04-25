9349f2062f7d9b14b2cf50f00fe3cd67428c8c82
Ganijon <Ganijon@mum.edu>
2017-04-25 00:48:23 -0500

    Added setup method

--- projects/mapreduce/project/src/part2/PairsReducer.java
+++ projects/mapreduce/project/src/part2/PairsReducer.java
@@ -14,29 +14,29 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import parts.WordPair;
 
-public class PairsReducer extends
-		Reducer<WordPair, IntWritable, WordPair, Text> {
+public class PairsReducer extends Reducer<WordPair, IntWritable, WordPair, Text> {
 
-	private int total = 0;
+	private int total;
 
 	@Override
-	public void reduce(WordPair pair, Iterable<IntWritable> counts,
-			Context context) throws IOException, InterruptedException {
-
+	protected void setup(Reducer<WordPair, IntWritable, WordPair, Text>.Context context) throws IOException, InterruptedException {
+		super.setup(context);
+		total = 0;
+	}
+	
+	@Override
+	public void reduce(WordPair pair, Iterable<IntWritable> counts, Context context) throws IOException, InterruptedException {
 		int sum = sum(counts);
-
 		if (pair.getNeighbor().equals(new Text("*"))) {
 			total = sum;
 		} else {
 			String relativeFrequency = sum + "/" + total;
 			context.write(pair, new Text(relativeFrequency));
 		}
-
 	}
 	
 	private int sum(Iterable<IntWritable> values) {
 		int sum = 0;
-
 		for (IntWritable val : values) {
 			sum += val.get();
 		}
