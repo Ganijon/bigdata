290ed325b3ac3e428aa1a04a2957e7b81eb32c35
Ganijon <Ganijon@172.17.4.251>
2017-04-25 01:36:19 -0500

    rm blank lines

--- projects/mapreduce/project/src/part3/StripesMapper.java
+++ projects/mapreduce/project/src/part3/StripesMapper.java
@@ -20,52 +20,35 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import parts.Stripe;
 
-public class StripesMapper extends
-		Mapper<LongWritable, Text, Text, Stripe> {
-
+public class StripesMapper extends Mapper<LongWritable, Text, Text, Stripe> {
 	private Stripe occurenceMap = new Stripe();
 
 	@Override
-	public void map(LongWritable key, Text line, Context context)
-			throws IOException, InterruptedException {
-
+	public void map(LongWritable key, Text line, Context context) throws IOException, InterruptedException {
 		List<String> words = Arrays.asList(line.toString().split("\\s"));
-
 		int wordIndex = 0;
-
 		for (String word : words) {
-
 			occurenceMap.clear();
-
 			for (String neighbor : getNeighbors(words, wordIndex)) {
-
 				IntWritable neighborCount = new IntWritable(1);
-
 				if (occurenceMap.containsKey(neighbor)) {
 					neighborCount = (IntWritable) occurenceMap.get(neighbor);
 					neighborCount.set(1 + neighborCount.get());
 				}
 				occurenceMap.put(new Text(neighbor), neighborCount);
-
 			}
-
 			wordIndex++;
-
 			context.write(new Text(word), occurenceMap);
 		}
 	}
 
 	private List<String> getNeighbors(List<String> words, int wordIndex) {
-
 		List<String> neighbors = new ArrayList<>();
-
 		for (int i = wordIndex + 1; i < words.size(); i++) {
-			if (words.get(wordIndex).equals(words.get(i))) {
+			if (words.get(wordIndex).equals(words.get(i))) 
 				break;
-			}
 			neighbors.add(words.get(i));
 		}
-
 		return neighbors;
 	}
 }
--- projects/mapreduce/project/src/part3/StripesReducer.java
+++ projects/mapreduce/project/src/part3/StripesReducer.java
@@ -17,39 +17,29 @@ import parts.Stripe;
 public class StripesReducer extends Reducer<Text, Stripe, Text, Stripe> {
 
 	@Override
-	public void reduce(Text word, Iterable<Stripe> stripes, Context context)
-			throws IOException, InterruptedException {
-
+	public void reduce(Text word, Iterable<Stripe> stripes, Context context) throws IOException, InterruptedException {
 		Stripe stripe = new Stripe();
-
 		for (Stripe s : stripes) {
 			mergeStripes(stripe, s);
 		}
-
 		divideByTotal(stripe, getTotal(stripe));
-
 		context.write(word, stripe);
 	}
 
 	private void mergeStripes(Stripe stripe, Stripe s) {
-
 		for (Writable neighbor : s.keySet()) {
 			IntWritable neighborCount = (IntWritable) s.get(neighbor);
-
 			if (stripe.containsKey(neighbor)) {
 				IntWritable count = (IntWritable) stripe.get(neighbor);
 				neighborCount.set(neighborCount.get() + count.get());
 			}
-			
 			stripe.put(neighbor, neighborCount);
 		}
 	}
 
 	private void divideByTotal(Stripe stripe, int total) {
-
 		Text newValue = new Text();
 		IntWritable oldValue;
-
 		for (Writable key : stripe.keySet()) {
 			oldValue = (IntWritable) stripe.get(key);
 			newValue.set(oldValue.toString() + "/" + String.valueOf(total));
@@ -63,5 +53,4 @@ public class StripesReducer extends Reducer<Text, Stripe, Text, Stripe> {
 			total += ((IntWritable) stripe.get(key)).get();
 		return total;
 	}
-
 }
